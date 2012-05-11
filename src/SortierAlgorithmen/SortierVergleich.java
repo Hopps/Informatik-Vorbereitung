26d3ac195bbfc416ffca4e9ffeb841fdfb9980f4
merlin <merlin@localhost>
2012-05-11 15:42:27 +0200

    + SortierAlgorithmen

---
+++ src/SortierAlgorithmen/SortierVergleich.java
@@ -0,0 +1,13 @@
+/*
+ * Hier sind die verschiedenen Sortieralgorithmen implementiert.
+ * Darüber hinaus werden sie bezüglich der Laufzeit verglichen.
+ */
+package SortierAlgorithmen;
+
+/**
+ *
+ * @author merlin
+ */
+public class SortierVergleich {
+    
+}
