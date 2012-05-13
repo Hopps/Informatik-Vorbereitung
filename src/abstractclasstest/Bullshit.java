c6b1f3176e29410bb2520aa60de0f2d81eee5326
merlin <merlin@localhost>
2012-05-13 18:37:38 +0200

    Wooo?

--- src/abstractclasstest/Bullshit.java
+++
@@ -1,9 +0,0 @@
-package abstractclasstest;
-
-public class Bullshit {
-
-    public static void main(String[] args) {
-        Hund hund = new Hund();
-        hund.abstrakteMethode();
-    }
-}
--- src/abstractclasstest/Tier.java
+++
@@ -1,7 +0,0 @@
-package abstractclasstest;
-
-public abstract class Tier {
-
-    public abstract void abstrakteMethode();
-
-}
